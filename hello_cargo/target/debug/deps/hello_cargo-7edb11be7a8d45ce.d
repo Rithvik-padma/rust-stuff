74739a62b7eb0da173ec0c431adc0b2bd7badd6f
Rithvik-padma <rithvik.padma@gmail.com>
2023-11-08 21:16:22 +0530

    add the hello world program using cargo tool

---
+++ hello_cargo/target/debug/deps/hello_cargo-7edb11be7a8d45ce.d
@@ -0,0 +1,5 @@
+C:\Rust\hello_cargo\target\debug\deps\hello_cargo-7edb11be7a8d45ce.rmeta: src\main.rs
+
+C:\Rust\hello_cargo\target\debug\deps\hello_cargo-7edb11be7a8d45ce.d: src\main.rs
+
+src\main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+C:\Rust\hello_cargo\target\debug\hello_cargo.exe: C:\Rust\hello_cargo\src\main.rs

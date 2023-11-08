5c1aa0c3e3e528796895bac5979719fca3b5ebba
Rithvik-padma <rithvik.padma@gmail.com>
2023-11-08 21:21:26 +0530

    ignore some files

--- hello_cargo/target/debug/deps/hello_cargo-7edb11be7a8d45ce.d
+++
@@ -1,5 +0,0 @@
-C:\Rust\hello_cargo\target\debug\deps\hello_cargo-7edb11be7a8d45ce.rmeta: src\main.rs
-
-C:\Rust\hello_cargo\target\debug\deps\hello_cargo-7edb11be7a8d45ce.d: src\main.rs
-
-src\main.rs:

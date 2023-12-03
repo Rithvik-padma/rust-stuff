a6d85a815b55044707bd63d06e9e1c2b059553b9
Rithvik-padma <rithvik.padma@gmail.com>
2023-12-03 18:22:55 +0530

    feat: add loop for invalid input and some essential prompts

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -7,7 +7,7 @@ fn main() {
 
     let secret_number: u32 = rand::thread_rng().gen_range(1..=99);
 
-    loop {
+    'guessNumber: loop {
         println!("\nInput your guess:");
 
         let mut guess: String = String::new();
@@ -23,41 +23,53 @@ fn main() {
             },
         };
         
-        println!("You guessed: {guess}");
+        // println!("You guessed: {guess}");
 
         match guess.cmp(&secret_number) {
             Ordering::Less => {
                 println!("Small!");
-                println!("Would you like to try again? (Y/N)");
             },
             Ordering::Greater => {
                 println!("Large!");
-                println!("Would you like to try again? (Y/N)");
             },
             Ordering::Equal => {
                 println!("You got it right!");
                 break;
             }
         }
-        
-        let mut to_continue: String = String::new();
 
-        io::stdin()
-            .read_line(&mut to_continue)
-            .expect("Failed to read your continuation opinion!"); 
-        
-        if !((to_continue.trim() == "Y") || (to_continue.trim() == "y")) {
-            println!("Do you want to know the number? (Y/N)");
-            let mut opinion: String = String::new();
+        'toContinue: loop {
+            println!("\nDo you want to try again? (Y/N)");
+
+            let mut to_continue: String = String::new();
 
             io::stdin()
-                .read_line(&mut opinion)
-                .expect("Failed to read your opinion!");
+            .read_line(&mut to_continue)
+            .expect("Failed to read your continuation opinion!");
+        
+            if !((to_continue.trim() == "Y") || (to_continue.trim() == "y")) {
+                if !((to_continue.trim() == "N") || (to_continue.trim() == "n")) {
+                    println!("Invalid input, please try again!");
+                    continue 'toContinue;
+                }
 
-            if (opinion.trim() == "Y") || (opinion.trim() == "y") {
-                println!("The correct number is: {secret_number}");
+                println!("Do you want to know the number? (Y/N)");
+                let mut opinion: String = String::new();
+
+                io::stdin()
+                    .read_line(&mut opinion)
+                    .expect("Failed to read your opinion!");
+
+                if (opinion.trim() == "Y") || (opinion.trim() == "y") {
+                    println!("The correct number is: {secret_number}");
+                }
+                else {
+                    println!("Thank you!");
+                }
+                break 'guessNumber;
             }
-            break;
         }
+ 
+        
     }
 }

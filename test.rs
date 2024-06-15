f86bd019cff9361105bea61dfaac22bddeea8e49
Rithvik <rithvik.padma@gmail.com>
2024-06-15 19:27:53 +0530

    borrowing anfd references

--- test.rs
+++ test.rs
@@ -21,4 +21,63 @@ fn main(){
     for ele in (1..4).rev() {
         println!("{ele}");
     }
+
+    let s = String::from("Hey Rithvik!");
+    println!("The length of first word is : {}", first_word_index(&s));
+
+
+    // string slice introduction
+
+    let greet = &s[..3];
+    let name = &s[4..(s.len()-1)];
+    println!("Greet: {} and Name: {}", greet, name);
+
+    println!("The first word in the string is: {}", first_word(&s));
+    println!("The second word in the string is: {}", second_word(&s));
+}   
+
+fn first_word(s: &String) -> &str{
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[..i];
+        }
+    }
+    &s[..]
+}
+
+fn second_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+    let mut count = 0;
+    let mut data = (0, 0);
+    for (i, &item) in bytes.iter().enumerate(){
+        if item == b' '{
+            count+=1;
+            if count==1 {data.0 = i;}
+            if count==2 {
+                data.1 = i;
+                break;
+            }
+        }
+    }
+    if count == 2{
+        return &s[(1+data.0)..data.1];
+    }else if count == 1{
+        return &s[(1+data.0)..];
+    }else {
+        return &"";
+    }
+}
+
+
+fn first_word_index(s: &String) -> usize {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return i;
+        }
+    }
+    s.len()
 }
